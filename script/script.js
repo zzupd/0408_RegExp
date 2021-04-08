0ea368c06ea15887fa4676aebfc675e2055068f6
zzupd <zzupd@naver.com>
2021-04-08 13:49:26 +0900

    0408_영어/숫자/특수기호(_ $) 유효성검사

--- script/script.js
+++ script/script.js
@@ -6,14 +6,14 @@ $(function () {
         chkData = chkData.trim(); // 앞뒤 공백제거
 
         // 비교 기준
-        var pattern = "영어/숫자 유효성검사";
+        var pattern = "영어/숫자/_/$  검사";
                      // (=영어와 숫자만 입력가능)
-        var regExp =  /[^a-z|A-Z|0-9]/;
+        var regExp =  /[^a-z|A-Z|0-9|_$]/;
         var res = "";
         if (regExp.test(chkData)) {
-            res = "영어/숫자만 입력가능";
+            res = "영/숫/_/$ 만 입력가능";
         } else {
-            res = "영어/숫자 OK";
+            res = "영/숫/_/$ OK";
         }
 
         $("#chkPattern").text("패턴(비교기준) : " + pattern);
